4eb4307ea110d5f256636a181a5811bbb16fd48a
qinj1070 <qinj1070@CA-SU-F106-23.sduhsd.lan>
2018-02-05 14:39:13 -0800

    added smiley face and stars+stripes

---
+++ APCSA/src/Unit1/SmileyFace.java
@@ -0,0 +1,38 @@
+package Unit1;
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Canvas;
+
+public class SmileyFace extends Canvas
+{
+   public SmileyFace()    //constructor - sets up the class
+   {
+      setSize(800,600);
+      setBackground(Color.WHITE);
+      setVisible(true);
+   }
+
+   public void paint( Graphics window )
+   {
+      smileyFace(window);
+   }
+
+   public void smileyFace( Graphics window )
+   {
+      window.setColor(Color.BLUE);
+      window.drawString("SMILEY FACE LAB ", 35, 35);
+
+      window.setColor(Color.YELLOW);
+      window.fillOval( 210, 100, 400, 400 );
+      
+      window.setColor(Color.BLACK);
+      window.fillOval(300, 200, 50, 50 );
+      window.fillOval(470, 200, 50, 50 );
+      window.drawArc(300, 250, 220, 145, 225, 90);
+
+      //add more code here
+
+
+   }
+}
