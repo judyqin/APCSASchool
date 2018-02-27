f2ea5703a1a4cc070ce52523576b645f99e49079
qinj1070 <qinj1070@CA-SU-F106-23.sduhsd.lan>
2018-02-27 14:59:24 -0800

    journal

---
+++ APCSA/src/Journal/Duplicate.java
@@ -0,0 +1,45 @@
+package Journal;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Duplicate {
+	
+	/*public static String numlist(int[] numbers)
+	{
+		String s = "";
+			for (int i = 0; i < numbers.length; i++)
+			{
+				s += numbers[i];
+			}
+		return s; 
+	}*/
+	
+	public static String Eliminated(int[] numbers)
+	{
+		String list = "";
+		
+		for (int i = 0; i < numbers.length; i++)
+		{
+			for (int j = i+1; j < numbers.length; j++)
+			{
+				if (numbers[i] != numbers[j])
+				{
+					list += numbers[i];
+				}
+			}
+		}
+		
+		return list; 
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		int[] test = {1, 2, 3, 2, 1, 6, 3, 4, 5, 2};
+		System.out.println(Arrays.toString(test)); 
+		System.out.println(Eliminated(test));
+		
+	}
+	
+}
